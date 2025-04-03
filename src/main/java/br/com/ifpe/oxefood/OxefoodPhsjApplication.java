756ecddc86a1121a4d1c5b57fd40bac3514c0e75
teste <teste@gmail.com>
2025-04-02 21:10:37 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodPhsjApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodPhsjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodPhsjApplication.class, args);
+	}
+
+}
